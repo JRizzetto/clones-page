0b8450087db3f1c1bb66356b400656631643773e
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-02-10 18:21:49 -0300

    Seção carrossel de stories

--- instagram-feed-clone/script.js
+++ instagram-feed-clone/script.js
@@ -1,14 +1,37 @@
-const heartIcon = document.getElementById("heart");
 
-heartIcon.addEventListener("click", () => {
-  // Alternar entre os estilos de coração
-  heartIcon.classList.toggle("fa-solid");
-  heartIcon.classList.toggle("fa-regular");
-  heartIcon.classList.toggle("solid");
+document.addEventListener("DOMContentLoaded", () => {
+// Curtida
+const heartIcon = document.querySelector(".heart-icon");
+  if (heartIcon) {
+    heartIcon.addEventListener("click", () => {
+      heartIcon.classList.toggle("fa-solid");
+      heartIcon.classList.toggle("fa-regular");
+      heartIcon.classList.toggle("solid");
 
-  // Adiciona a classe de animação e a remove após 300ms (tempo da animação)
-  heartIcon.classList.add("pulse");
+      heartIcon.classList.add("pulse");
+      setTimeout(() => {
+        heartIcon.classList.remove("pulse");
+      }, 300);
+    });
+  } else {
+    console.error("Elemento heart não encontrado!");
+  }
+
+// Carregamento barra dos stories
+  console.log("Página carregada!");
+  const progress = document.querySelector(".progress"); // Corrigido
   setTimeout(() => {
-    heartIcon.classList.remove("pulse");
-  }, 300);
+    progress.style.width = "100%"; // Agora está certo
+  }, 100);
 });
+
+//Pausar stories 
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
+function togglePlayPause() {
+  playButton.classList.toggle("hidden-i");
+  pauseButton.classList.toggle("hidden-i");
+}
+playButton.addEventListener("click", togglePlayPause);
+pauseButton.addEventListener("click", togglePlayPause);
+
